4ce1bfb4c2b37ff8088b4cb9f887017905df4b7e
HuuTho <huu.tho.ns@gmail.com>
2023-02-22 16:28:47 +0700

    git  page

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+const { REACT_APP_TEST, REACT_APP_TEST2 } = process.env;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>ENV 1:{REACT_APP_TEST}</p>
+        <p>ENV 1:{REACT_APP_TEST2}</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
