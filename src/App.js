5021daca8274e19f5808da8f8d0511010333ba34
HuuTho <huu.tho.ns@gmail.com>
2023-02-22 16:28:58 +0700

    page 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-const { REACT_APP_TEST, REACT_APP_TEST2 } = process.env;
+const { REACT_APP_TEST, REACT_APP_TEST2, REACT_APP_TEST3 } = process.env;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>ENV 1:{REACT_APP_TEST}</p>
-        <p>ENV 1:{REACT_APP_TEST2}</p>
+        <p>ENV 2:{REACT_APP_TEST2}</p>
+        <p>ENV 3:{REACT_APP_TEST3}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
